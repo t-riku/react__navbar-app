827bccd1cdf73bce6e5c7e0cc054c7f397a71426
t-riku <dingariku@icloud.com>
2022-06-23 14:30:39 +0900

    tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Gaming from "./components/Gaming";
+import Home from "./components/Home";
+import Learning from "./components/Learning";
+import Live from "./components/Live";
+import Movies from "./components/Movies";
+import Music from "./components/Music";
+import News from "./components/News";
+import Sports from "./components/Sports";
+import Trending from "./components/Trending";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <div className="w-11/12 h-5/6 md:w-3/4">
+          <BrowserRouter>
+            <div>こんにちは</div>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/gaming" element={<Gaming />}></Route>
+              <Route path="/learning" element={<Learning />}></Route>
+              <Route path="/live" element={<Live />}></Route>
+              <Route path="/movies" element={<Movies />}></Route>
+              <Route path="/music" element={<Music />}></Route>
+              <Route path="/news" element={<News />}></Route>
+              <Route path="/sports" element={<Sports />}></Route>
+              <Route path="/trending" element={<Trending />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </>
   );
 }
 
